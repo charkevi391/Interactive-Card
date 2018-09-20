f7718c555089e0d0add10b9a0d53b88ee6de2261
charkevi391 <charkevi391@ed.amdsb.ca>
2018-09-20 15:00:13 -0400

    Drew Initial Shapes

--- Interactive Card/Form1.cs
+++ Interactive Card/Form1.cs
@@ -11,6 +11,11 @@ using System.Media;
 using System.Threading;
 
 namespace Interactive_Card
+
+    ///Designed By: Charlie Kevill
+    ///Date: Sept 2018
+    ///Description: An Interactive Shape Day card with an animated joke inside
+
 {
     public partial class Form1 : Form
     {
@@ -24,23 +29,40 @@ namespace Interactive_Card
             Graphics g = this.CreateGraphics();
             Font drawFont = new Font("TimesNewRoman", 16, FontStyle.Bold);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
-            SolidBrush darkBlueBrush = new SolidBrush(Color.BlueViolet);
-
+            SolidBrush blueBrush = new SolidBrush(Color.BlueViolet);
+            SolidBrush yellowBrush = new SolidBrush(Color.YellowGreen);
+            Pen drawPen = new Pen(Color.YellowGreen, 10);
+            Pen mouthPen = new Pen(Color.Black,8);
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
+            Graphics g = this.CreateGraphics();
+            Font drawFont = new Font("TimesNewRoman", 16, FontStyle.Bold);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush blueBrush = new SolidBrush(Color.BlueViolet);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            Pen drawPen = new Pen(Color.YellowGreen, 10);
+            Pen mouthPen = new Pen(Color.Black, 2);
+
             this.BackgroundImage = null;
             this.startButton.Visible = false;
             Refresh();
-
-            Graphics g = this.CreateGraphics();
 
             g.Clear(Color.Aquamarine);
 
             Thread.Sleep(500);
 
-            g.FillRectangle(darkBlueBrush, 30, 30, 100, 200);
+            //Initial Drawing of Shapes 
+            g.DrawLine(drawPen, 0, 305, 550, 305);
+            g.FillRectangle(blueBrush, 50, 200, 100, 100);
+            g.FillEllipse(yellowBrush, 250, 200, 100, 100);
+            g.FillEllipse(blackBrush, 300, 230, 12, 12);
+            g.FillEllipse(blackBrush, 270, 230, 12, 12);
+            g.FillEllipse(blackBrush, 110, 230, 12, 12);
+            g.FillEllipse(blackBrush, 80, 230, 12, 12);
+            g.DrawEllipse(mouthPen, 290, 260, 10, 20);
+            g.DrawEllipse(mouthPen, 90, 260, 20, 10);
 
         }
     }
